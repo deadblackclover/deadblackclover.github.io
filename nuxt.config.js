f27c99ceac0fd79639e646e6846752aa08794dba
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-09-11 22:24:41 +0700

    Add files

---
+++ nuxt.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    head: {
+        titleTemplate: 'DEADBLACKCLOVER',
+        meta: [
+            {charset: 'utf-8'},
+            {'http-equiv': 'X-UA-Compatible', content: 'IE=edge'}
+        ]
+    },
+    css: [
+        {src:'~/assets/css/main.css'},
+        {src:'~/assets/css/animate.css'}
+    ]
+}
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="layout">
+		<div class="main">
+			<div class="logo">
+				DEADBLACKCLOVER
+			</div>
+			<div class="links animated flash infinite slower">
+				<a href="https://gitlab.com/deadblackclover">
+					<div class="img gitlab"></div>
+				</a>
+			</div>
+			<div class="stack animated flash infinite slower">
+				<div class="img html5"></div>
+				<div class="img css3"></div>
+				<div class="img javascript"></div>
+				<div class="img docker"></div>
+				<div class="img blockchain"></div>
+			</div>
+		</div>
+	</div>
+</template>

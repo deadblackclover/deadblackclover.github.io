7e99f53982bd30433576f4fbca52437b787da65b
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2018-10-01 00:56:07 +0700

    Change header

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,44 @@
 <template>
 	<div class="layout">
-		<div class="main">
-			<div class="logo">
+		<header>
+			<h1 class="logo">
 				DEADBLACKCLOVER
+			</h1>
+			<h3>
+				Web and Javascript Developer
+			</h3>
+			<div class="icons">
+				<div class="stack animated flash infinite slower">
+					<img class="img" src="~/assets/img/icons8-html-5.png">
+					<img class="img" src="~/assets/img/icons8-css3.png">
+					<img class="img" src="~/assets/img/icons8-javascript.png">
+					<img class="img" src="~/assets/img/icons8-docker.png">
+					<img class="img" src="~/assets/img/icons8-gitlab.png">
+				</div>
 			</div>
-			<div class="links animated flash infinite slower">
-				<a href="https://gitlab.com/deadblackclover">
-					<div class="img gitlab"></div>
-				</a>
-			</div>
-			<div class="stack animated flash infinite slower">
-				<div class="img html5"></div>
-				<div class="img css3"></div>
-				<div class="img javascript"></div>
-				<div class="img docker"></div>
-				<div class="img blockchain"></div>
-			</div>
-		</div>
+		</header>
+		<main>
+			<section>
+				<h3>
+					Projects
+				</h3>
+				<div class="items">
+					<div class="items_item">lofiTorrent</div>
+					<div class="items_item">Ames</div>
+					<div class="items_item">LTTC</div>
+				</div>
+				<a href="https://gitlab.com/deadblackclover">More project</a>
+			</section>
+			<section>
+				<h3>
+					Link
+				</h3>
+				<div class="items">
+					<div class="items_item">NPM</div>
+					<div class="items_item">PixelFed</div>
+					<div class="items_item">Friendica</div>
+				</div>
+			</section>
+		</main>
 	</div>
 </template>
